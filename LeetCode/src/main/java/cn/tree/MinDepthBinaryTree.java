fca7357fba83392395d4e4082f9fa8b65345f4f4
superman-wrdh <hczwbs19931015>
2019-06-14 22:59:30 +0800

    min tree depth

--- LeetCode/src/main/java/cn/tree/MinDepthBinaryTree.java
+++ LeetCode/src/main/java/cn/tree/MinDepthBinaryTree.java
@@ -8,31 +8,14 @@ package cn.tree;
  * TreeNode right;
  * TreeNode(int x) { val = x; }
  * }
- *
  */
 // TODO https://leetcode.com/problems/minimum-depth-of-binary-tree/submissions/
 public class MinDepthBinaryTree {
     public int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        } else if (root.left == null || root.right == null) {
-            return 1;
-        }
-        int dl = depth(root.left);
-        int dr = depth(root.right);
-        if (dl == 0 || dr == 0) {
-            return Math.max(dl, dr);
-        }
-        return Math.min(dl, dr) + 1;
-    }
-
-    public int depth(TreeNode root) {
-        if (null == root) {
-            return 0;
-        } else {
-            int lDepth = depth(root.left);
-            int rDepth = depth(root.right);
-            return lDepth > rDepth ? lDepth + 1 : rDepth + 1;
-        }
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return 1 + minDepth(root.right);
+        if (root.right == null) return 1 + minDepth(root.left);
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
