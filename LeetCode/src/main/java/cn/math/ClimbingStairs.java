a794518a80891dcc2b7815296b712f7899222fe6
superman-wrdh <hczwbs19931015>
2019-06-14 23:18:35 +0800

    ClimbingStairs

---
+++ LeetCode/src/main/java/cn/math/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package cn.math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        return help(cache, n);
+    }
+
+    public int help(Map<Integer, Integer> cache, int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        Integer c = cache.get(n);
+        if (c != null) return c;
+        int value = help(cache, n - 1) + help(cache, n - 2);
+        cache.put(n, value);
+        return value;
+    }
+
+}
