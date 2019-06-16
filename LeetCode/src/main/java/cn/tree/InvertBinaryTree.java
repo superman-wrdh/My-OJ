c1d1f98c462f3306dcbf681b5b9dca875f8b57b9
superman-wrdh <13419655360@163.com>
2019-06-16 23:52:37 +0800

    invert BFS

---
+++ LeetCode/src/main/java/cn/tree/InvertBinaryTree.java
@@ -0,0 +1,21 @@
+package cn.tree;
+
+/**
+ * Created by super on 2019/6/16
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+//https://leetcode.com/problems/invert-binary-tree/
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+}
