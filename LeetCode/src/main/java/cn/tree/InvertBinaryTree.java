7e7f580592823135efd73155af773ee016e9f5db
superman-wrdh <13419655360@163.com>
2019-06-17 00:03:25 +0800

    invert BFS

--- LeetCode/src/main/java/cn/tree/InvertBinaryTree.java
+++ LeetCode/src/main/java/cn/tree/InvertBinaryTree.java
@@ -14,8 +14,22 @@ package cn.tree;
  * }
  */
 //https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Success
+ * Details
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
+ * Memory Usage: 34.2 MB, less than 100.00% of Java online submissions for Invert Binary Tree.
+ */
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null) return null;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
