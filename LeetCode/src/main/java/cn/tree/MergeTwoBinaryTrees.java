37123e0b45911f98c357263b82c5049fc5acddbc
superman-wrdh <13419655360@163.com>
2019-07-01 22:26:58 +0800

    MergeTwoBinaryTrees

---
+++ LeetCode/src/main/java/cn/tree/MergeTwoBinaryTrees.java
@@ -0,0 +1,14 @@
+package cn.tree;
+
+//https://leetcode.com/problems/merge-two-binary-trees/submissions/
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
+}
