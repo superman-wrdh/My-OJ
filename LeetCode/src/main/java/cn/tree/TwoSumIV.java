4d87d0d9467b16eb3955fd1913dba21fe9c0abc7
superman-wrdh <13419655360@163.com>
2019-07-03 22:48:57 +0800

    TwoSumIV

--- LeetCode/src/main/java/cn/tree/TwoSumIV.java
+++ LeetCode/src/main/java/cn/tree/TwoSumIV.java
@@ -3,8 +3,52 @@ package cn.tree;
 /**
  * Created by super on 2019/7/3
  * https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+ * Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+ *
+ * Example 1:
+ *
+ * Input:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 9
+ *
+ * Output: True
+ *
+ *
+ * Example 2:
+ *
+ * Input:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 28
+ *
+ * Output: False
  */
 public class TwoSumIV {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+
+        System.out.println(new TwoSumIV().findTarget(root,9));
+        System.out.println(new TwoSumIV().findTarget(root,28));
+
+    }
+
+
     public boolean findTarget(TreeNode root, int k) {
         return preTraverse(root, root, k);
     }
